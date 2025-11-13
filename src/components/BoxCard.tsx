ab55549105e229689b0a9363c4b8f45ba847944f
Navya Nandani <navya_nandani@berkeley.edu>
2025-11-12 22:28:22 -0800

    heading on home page, gallery page updated

--- src/components/BoxCard.tsx
+++ src/components/BoxCard.tsx
@@ -7,8 +7,12 @@ type Props = {
 
 export default function BoxCard({ title, image }: Props) {
   return (
-    <div className="relative rounded-2xl overflow-hidden shadow-md hover:shadow-xl transition-transform duration-300 hover:scale-[1.02] aspect-square">
+    <div className="relative rounded-2xl overflow-hidden shadow-md hover:shadow-xl 
+        transition-transform duration-300 hover:scale-[1.02] aspect-square 
+        max-w-[400px] mx-auto">
+
       <img src={image} alt={title} className="w-full h-full object-cover" />
+
       <div className="absolute inset-0 bg-black/15 flex items-center justify-center">
         <h3 className="text-white text-2xl font-semibold text-center">
           {title}
